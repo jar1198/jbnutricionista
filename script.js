78f44535237bedb2c790f63e2da32ced1b1e1baf
jar1198 <abacaxi1198@gmail.com>
2025-05-21 15:58:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
-document.querySelectorAll('.faq-question').forEach(button => {
-  button.addEventListener('click', () => {
-    const answer = button.nextElementSibling;
+document.querySelectorAll('.faq-question').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const answer = btn.nextElementSibling;
     answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
   });
 });
